f86e4ae0bd9093ab5915295f29df391cd5302bf2
Portus74 <nelson.jesus.pt@gmail.com>
2025-01-25 12:28:36 +0000

    variosPrints

--- 20250125/printMeuNome.py
+++ 20250125/printMeuNome.py
@@ -1,3 +1,17 @@
 #imprimir o meu nome no ecran
 print("Nelson Jesus")
 print("Estou a frequentar formação de PYTHON pela EISNT")
+
+num = input("Digite o ano nascimento: ")
+print("Nasceu no ano", num)
+
+print("Ano nascimento =", num,",\no quadrado do ano nascimento = ", int(num) ** 2)
+
+#formatação posicional %d para inteiro, %s string, %f para decimal
+print("Ano nascimento = %d,\no quadrado do ano nascimento = %f"%(int(num), round(int(num) ** 2, 0)))
+
+#usando o format
+print("Ano nascimento ={},\no quadrado do ano nascimento = {}".format(num, int(num) ** 2))
+
+#mais atual e mais eficiente
+print(f"Ano nascimento ={num}\no quadrado do ano nascimento = {int(num) ** 2}")
