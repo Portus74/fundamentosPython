099762d9722c487738953d327e67ee58d65a4b2a
Portus74 <nelson.jesus.pt@gmail.com>
2025-02-22 10:50:30 +0000

    Ex01

--- UFCD10793/FICHAS/FT05/ex07.py
+++ UFCD10793/FICHAS/FT05/ex07.py
@@ -8,4 +8,31 @@ Crie um programa para controlar listas, com as seguintes funções:
 * Imprimir elementos da lista
 * Esvaziar lista
 '''
+lista = []
+
+lista.insert(0, 10)
+print(f"{10} adicionado no início.")
+
+lista.append(20)
+print(f"{20} adicionado no fim.")
+
+lista.append(30)
+print(f"{30} adicionado no fim.")
+
+print("Elementos da lista:", lista)
+
+if 20 in lista:
+    lista.remove(20)
+    print(f"{20} removido.")
+else:
+    print(f"{20} não encontrado na lista.")
+
+print(f"Tamanho da lista: {len(lista)}")
+
+print("Elementos da lista:", lista)
+
+lista.clear()
+print("Lista esvaziada.")
+
+print("Elementos da lista:", lista)
 
